1c90d06d385919dd0384dd6f0ff1110cac80cc71
Ben Grant <bgrant@olin.edu>
2021-03-29 15:44:20 -0400

    Added some data analysis and plotting for total crashes by year over time and total crashes by month.
    added csv data file for world total flights from 2000-2020

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,9 +1,29 @@
 import dateparser as dp
+import pandas as pd
+import matplotlib.pyplot as plt
 
-date1 = "March 29 2021"
+# date1 = "October 01 1916"
 
-parsed = dp.parse(date1)
+# parsed = dp.parse(date1)
 
-p_year = parsed.year
+# print(parsed)
 
-print(type(p_year))
+# print(parsed.year, "\n", parsed.month, "\n", parsed.day, "\n")
+
+dates = pd.read_csv("all_data.csv")["Date:"]
+
+crashes_by_year = {}
+
+for date in dates:
+    # if crashes_by_year
+
+    # use date parser on date:
+    year = dp.parse(date).year
+
+    if year in crashes_by_year:
+        crashes_by_year[year] += 1
+    else:
+        crashes_by_year[year] = 1
+
+plt.plot(crashes_by_year.keys(), crashes_by_year.values())
+plt.show()
